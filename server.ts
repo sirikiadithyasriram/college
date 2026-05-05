61238c6abeeb9d6ab9e63160eddbbd795eb4d87a
sirikiadithyasriram <sirikiadithyasriram@gmail.com>
2026-05-05 16:39:42 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -47,7 +47,7 @@ async function startServer() {
 
   // 3️⃣ Start server AFTER DB initialized
   app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+    console.log(`Server running on http://0.0.0.0:3000`);
   });
 }
 
